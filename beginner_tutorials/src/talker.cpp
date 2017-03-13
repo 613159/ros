ccf3d4e8e78d271f663480e69de0db3b59b84ec0
hljchenxi <760921212@qq.com>
2017-03-13 01:45:54 -0700

    start

--- beginner_tutorials/src/talker.cpp
+++ beginner_tutorials/src/talker.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 int main(int argc, char **argv)
 {
+  argc=argc;
   /**
    * The ros::init() function needs to see argc and argv so that it can perform
    * any ROS arguments and name remapping that were provided at the command line. For programmatic
